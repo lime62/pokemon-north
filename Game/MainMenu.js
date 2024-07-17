343bd0874cfcc3d87aaefc9d09e0374462fd0e00
Margaret A. <margaret.acheampong.dev@gmail.com>
2024-07-17 18:10:59 -0400

    save menu feature is now done

--- Game/MainMenu.js
+++ Game/MainMenu.js
@@ -1,34 +1,45 @@
 const readlineSync = require('readline-sync');
-const GameState = require('../Save/GameState');
+let GameState = require('../Save/GameState');
+const saveFile = require('../Save/saveFile.json');
 
 function display(){
   GameState.menuOn = true;
   console.log("Pokemon North");
   const selection = readlineSync.question("New Game or Continue [N/C]: ");
-  const dataSaved = GameState.player && GameState.location;
+  const dataSaved = saveFile.player && saveFile.location;
 
   // If the player wants to continue, show them a snapshot of their progress if the game is saved.
   if (selection == "C"){
      if(dataSaved){
-       console.log(`Player: ${player.name}`);
-       console.log(`Badges: ${player.badges}`);
+       console.log(`Player: ${saveFile.player.name}`);
+       console.log(`Badges: ${saveFile.player.badges}`);
        start = readlineSync.question("Start game [Y]");
      }
      else{
       console.log("No saved data available.");
      }
   }
+  // If the player wants to start the new game check if they have previously had a save file
   else if (selection == "N"){
     if(dataSaved){
       overwriteData = readlineSync.question("There is saved data found. Would you like to overwrite this [Y/N]: ");
       if(overwriteData == "Y"){
+        // Reset their save file and start the introduction cutscene
         console.log("OVERWRITING DATA.");
+
+        //Reset their data saved
+        saveFile.player = {};
+        saveFile.location = "";
+
+        //Set their current progress (what they see at the moment)
+        GameState.player = {};
         GameState.location = "Introduction";
         GameState.menuOn = false;
         GameState.cutsceneOn = true;
       }
     }
     else{
+      // If they did not previously save, just start a new game
       GameState.location = "Introduction";
       GameState.menuOn = false;
       GameState.cutsceneOn = true;
---
+++ Menu/Save.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const GameState = require('../Save/GameState');
+const SavedState = require('../Save/SavedState');
+const readlineSync = require('readline-sync');
+
+function saveGame(){
+  //If the player has never saved before, save their game right away.
+  const dataSaved = SavedState.player && SavedState.location;
+  if (!dataSaved){
+    const saveFile = { player: GameState.player, location: GameState.location};
+    fs.writeFileSync('./Save/saveFile.json', JSON.stringify(saveFile));
+  }
+  else{
+    const answer = "Would you like to overwrite your previous saved file [Y/N]: ";
+    if (answer == "Y"){
+      const saveFile = { player: GameState.player, location: GameState.location};
+      fs.writeFileSync('./Save/saveFile.json', JSON.stringify(saveFile));
+      return saveFile;
+    }
+    else{
+      return -1;
+    }
+  }
+}
+
+module.exports = saveGame;
